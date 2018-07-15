5b4319a9dcc3b255416a98a01ca50fd655f26e26
Vinícius Eduardo da Silva Arré <viniciusarre@gmail.com>
2018-07-15 08:20:10 -0400

    More adjustments on profile photo

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Row, Col, Image} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import profile from '../img/personalPhoto.jpg';
 
 const About = ({about}) =>{
@@ -9,9 +9,9 @@ const About = ({about}) =>{
           <h1 className={"text-center"}>{title}</h1>
           <Grid>
               <Row>
-                  <Col xs={12} md={12} >
-                    <Col xs={12} md={6} style={{marginTop: '2%', marginLeft:'37%'}}>
-                        <Image style={{height:'20%', width:'50%', paddingBottom:'7%'}} responsive src={profile} alt={"Profile of Vinícius Arré, Full Stack developer"} />
+                  <Col xs={12} md={12} style={{marginTop: '2%'}} className={"pull-center"} >
+                    <Col xs={12} md={12} >
+                        <img  className="img-responsive" style={{margin:'0 auto', height:'10%', width:'25%', paddingBottom:'7%',}} src={profile} alt={"Profile of Vinícius Arré, Full Stack developer"} />
 
                     </Col>
 
